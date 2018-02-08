22a108047a6f45655f70a3a27d3dd1d467c52ff5
Stephen Shaw <sshaw@decriptor.com>
2018-02-08 12:10:38 -0700

    Some minor F# tweaks

--- CryptoMatrixFSharp/Crypto.fs
+++ CryptoMatrixFSharp/Crypto.fs
@@ -1,5 +1,6 @@
 ﻿module Crypto
 
+open System.Text
 open MathNet.Numerics.LinearAlgebra
 
 let codex =  Map ['a', 1.0; 'b', 2.0; 'c', 3.0; 'd', 4.0; 'e', 5.0; 'f', 6.0; 'g', 7.0;
@@ -12,36 +13,33 @@ let private encode = DenseMatrix.randomStandard<float> 3 3
 let private decode = encode.Inverse ()
 
 /// http://www.fssnip.net/5u/title/String-explode-and-implode
-/// Converts a string into a list of characters.
-let explode (s:string) =
-    [for c in s -> c]
-
 /// Converts a list of characters into a string.
-let implode (xs:float []) =
-    let xs = xs |> Array.map (fun x -> (Map.findKey (fun k v -> v = round x) codex))
-    let sb = System.Text.StringBuilder(xs.Length)
+let implode (xs:char []) =
+    let sb = StringBuilder(xs.Length)
     xs |> Array.iter (sb.Append >> ignore)
     sb.ToString().TrimEnd ()
 
+let intToCharacterArray (a:float[]) =
+    a |> Array.map (fun x -> (Map.findKey (fun k v -> v = round x) codex))
+
 let messageToArray (s:string) =
     let padding = encode.RowCount - (s.Length % encode.RowCount)
-    let sPadded = s.PadRight(s.Length + padding, ' ')
-    sPadded |> explode |> List.map (fun x -> codex.Item (x)) |> List.toArray
+    let paddedMessage = s.PadRight(s.Length + padding, ' ').ToCharArray()
+    paddedMessage |> Array.map (fun x -> codex.Item (x))
 
 let encodeMessage (message:string) =
     let messageArray = messageToArray message
     let columns = (messageArray.Length / encode.RowCount)
-    let M = Matrix<float>.Build
-    let messageMatrix = M.Dense (encode.RowCount, columns, messageArray)
+    let messageMatrix = Matrix<float>.Build.Dense (encode.RowCount, columns, messageArray)
     let encoded = encode.Multiply (messageMatrix)
 
     encoded.ToColumnMajorArray()
 
 let decodeMessage (encodedMessage:float[]) =
-    let columns = encodedMessage.Length / encode.RowCount;
-    let encodedMatrix = Matrix<float>.Build.DenseOfColumnMajor(encode.RowCount, columns, encodedMessage);
+    let columns = encodedMessage.Length / encode.RowCount
+    let encodedMatrix = Matrix<float>.Build.DenseOfColumnMajor(encode.RowCount, columns, encodedMessage)
 
-    let decodedMatrix = decode.Multiply(encodedMatrix);
-    let decodedArray = decodedMatrix.ToColumnMajorArray();
-
-    implode decodedArray
+    let decodedMatrix = decode.Multiply(encodedMatrix)
+    let decodedArray = decodedMatrix.ToColumnMajorArray()
+    let characterArray = intToCharacterArray (decodedArray)
+    implode characterArray
